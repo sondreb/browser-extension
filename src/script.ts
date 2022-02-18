ed58073bdf43c56c846d7ea59705f76bbc2883ec
SondreB <sondre@outlook.com>
2022-02-18 13:46:04 +0100

    Add button to verify all session storage APIs

--- src/script.ts
+++ src/script.ts
@@ -7,13 +7,17 @@ const state = {
     items: ["1", "2", "3"]
 };
 
+const store = {
+    items: ["1", "2", "3"]
+};
+
 document.getElementById('try-button')?.addEventListener("click", () => {
     console.log('Hello World!');
 });
 
 document.getElementById('set-button')?.addEventListener("click", async () => {
     const chr = chrome.storage as any;
-    await chr.session.set({ 'data': state });
+    await chr.session.set({ 'data': state, 'store': store });
     console.log('STATE SAVED!');
 
     const bytesInUse = await chr.session.getBytesInUse();
@@ -22,8 +26,26 @@ document.getElementById('set-button')?.addEventListener("click", async () => {
 
 document.getElementById('get-button')?.addEventListener("click", async () => {
     const chr = chrome.storage as any;
-    const data = await chr.session.get(['data', 'ui', 'action', 'store']);
-    console.log(data);
+    const data = await chr.session.get(['data', 'store']);
+    console.log('Data and Store', data);
+});
+
+document.getElementById('get-store-button')?.addEventListener("click", async () => {
+    const chr = chrome.storage as any;
+    const data = await chr.session.get(['store']);
+    console.log('Store', data);
+});
+
+document.getElementById('remove-button')?.addEventListener("click", async () => {
+    const chr = chrome.storage as any;
+    await chr.session.remove(['data']);
+    console.log('data was removed');
+});
+
+document.getElementById('clear-button')?.addEventListener("click", async () => {
+    const chr = chrome.storage as any;
+    await chr.session.clear();
+    console.log('session state was cleared');
 });
 
 
