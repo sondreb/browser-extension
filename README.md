ba2af0959a7738f882d135bfb31be3d508acc7a8
SondreB <sondre@outlook.com>
2022-02-18 13:41:05 +0100

    Add script to verify session set and get

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # browser-extension
 
-Testing grounds for build browser extension on Manifest V3
+Testing grounds for build browser extension on Manifest V3.
 
+## Requirements
+
+- Canary (nightly) build of Chrome or Edge.
+- Node.js
+- TypeScript
+
+## Purpose
+
+The purpose of this repo and extension is to verify certain patterns and functionality for Manifest V3 browser extensions.
+
+It relies on the new "chrome.storage.session" storage API that can be used to keep secrets, such as private keys and passwords.
+
+Please remember that browsers and extensions are inherently not very secure.
--- src/script.ts
+++ src/script.ts
@@ -2,15 +2,29 @@ function writeToLog(text: string) {
     console.log(text);
 }
 
+const state = {
+    activated: true,
+    items: ["1", "2", "3"]
+};
 
-const btn = document.getElementById('try-button');
+document.getElementById('try-button')?.addEventListener("click", () => {
+    console.log('Hello World!');
+});
 
-if (btn)
-{
-    btn.addEventListener("click", () => {
-        console.log('Hello World!');
-    });
-}
+document.getElementById('set-button')?.addEventListener("click", async () => {
+    const chr = chrome.storage as any;
+    await chr.session.set({ 'data': state });
+    console.log('STATE SAVED!');
+
+    const bytesInUse = await chr.session.getBytesInUse();
+    console.log(bytesInUse);
+});
+
+document.getElementById('get-button')?.addEventListener("click", async () => {
+    const chr = chrome.storage as any;
+    const data = await chr.session.get(['data', 'ui', 'action', 'store']);
+    console.log(data);
+});
 
 
-
+// {get: ƒ, set: ƒ, remove: ƒ, clear: ƒ, getBytesInUse: ƒ, …}
